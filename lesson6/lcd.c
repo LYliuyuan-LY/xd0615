fa7e453eeef4e8400ca9134f59ef659f238265c2
LYliuyuan-LY <1175468238@qq.com>
2020-06-21 14:00:58 +0800

    第六次作业 刘源

---
+++ lesson6/lcd.c
@@ -0,0 +1,21 @@
+
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup()
+{
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  lcd.setCursor(0, 0);
+  if(Serial.available()>0)
+  {
+    lcd.print(Serial.readString());
+  }
+}
+ 
